24102dc1d70578ea5eee05a520228de60f35f45a
chengJunG <cheng294656098@gmail.com>
2015-03-17 10:03:11 +0800

    initialization

---
+++ javase/src/main/java/com/cheng/ssy/javase/App.java
@@ -0,0 +1,13 @@
+package com.cheng.ssy.javase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
